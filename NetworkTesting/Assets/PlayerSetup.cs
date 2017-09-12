cefd6d43b8d9127c0c3324408361690d85b4eb62
JesseFolkertsma <jesse.folkertsma@gmail.com>
2017-09-12 09:46:38 +0200

    init

---
+++ NetworkTesting/Assets/PlayerSetup.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour {
+
+    [SerializeField]
+    Behaviour[] compToDisable;
+
+    [SerializeField]
+    Camera mainCamera;
+
+    private void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            for (int i = 0; i < compToDisable.Length; i++)
+            {
+                compToDisable[i].enabled = false;
+            }
+        }
+
+        else
+        {
+            mainCamera = Camera.main;
+            if (mainCamera != null)
+                mainCamera.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (mainCamera != null)
+            mainCamera.gameObject.SetActive(true);
+    }
+}
